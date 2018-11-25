d0bf99820c1505ea8c13c4c76d329735b24e50ff
Vinayaka Patrick Thompson <thompvin@oregonstate.edu>
2018-11-24 19:08:11 -0800

    tests with new hashing system

--- index.php
+++ index.php
@@ -9,7 +9,18 @@
     <?php include 'templates/header.php'; ?>
 	<?php include 'templates/sql.php'; ?>
 
-	<?php echo password_hash('rasmuslerdorf', PASSWORD_DEFAULT)."\n"?>
+  <?php 
+  $pass = 'rasmuslerdorf';
+  $notPass = 'lama';
+  $hash = password_hash($pass, PASSWORD_DEFAULT);
+  if (password_verify($notPass, $hash)) {
+    echo('True \n');
+  } else {
+    echo('False \n');
+  }
+  //echo password_hash('rasmuslerdorf', PASSWORD_DEFAULT)."\n";
+  
+  ?>
 		<div class="container">
     
 		</div>
