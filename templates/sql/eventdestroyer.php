beafe81cb66ddfd6ac982e19141e6b2b85735bdf
Vinayaka Patrick Thompson <thompvin@oregonstate.edu>
2018-11-24 19:32:20 -0800

    removed useless files

--- templates/sql/eventdestroyer.php
+++
@@ -1,7 +0,0 @@
-
-	<?php include 'templates/sqllogin.php'; 
-	
-	$sql = "DELETE FROM `2008597_thompvin`.`events` WHERE `events`.`eventid` = " .$_POST["Eventid"];
-        $ans = mysql_query($sql);
-		print "Event Deleted";
-		?>
